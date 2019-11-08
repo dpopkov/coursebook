fcbd2377004d2d24272a6e1316a4eb7593d9ed7d
Denis Popkov <pkvdenis@gmail.com>
2019-11-08 03:28:18 +0300

    move choice of disk to input layout

--- coursebookgui/src/main/java/org/daydevjv/coursebookgui/InputLayoutController.java
+++ coursebookgui/src/main/java/org/daydevjv/coursebookgui/InputLayoutController.java
@@ -1,12 +1,40 @@
 package org.daydevjv.coursebookgui;
 
-import java.io.IOException;
+import java.util.List;
+
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.util.StringConverter;
+import org.daydevjv.coursebook.model.FileStoreInfo;
+import org.daydevjv.coursebook.service.FileStoreUtils;
 
 public class InputLayoutController {
+    @FXML
+    public ChoiceBox<FileStoreInfo> choiceDisk;
+    @FXML
+    public Label lblSelectedDisk;
 
     @FXML
-    private void switchToSecondary() throws IOException {
-        App.setRoot("secondary");
+    void initialize() {
+        /* Top HBox children */
+        List<FileStoreInfo> info = FileStoreUtils.getInfo();
+        lblSelectedDisk.setText("Selected disk: " + info.get(0).getName());
+        choiceDisk.setConverter(new StringConverter<>() {
+            @Override
+            public String toString(FileStoreInfo fsInfo) {
+                return fsInfo.getName();
+            }
+
+            @Override
+            public FileStoreInfo fromString(String s) {
+                throw new UnsupportedOperationException("Converting from String to FileStoreInfo is not supported");
+            }
+        });
+        choiceDisk.setItems(FXCollections.observableList(info));
+        choiceDisk.getSelectionModel().selectFirst();
+        choiceDisk.setOnAction(event -> lblSelectedDisk.setText("Selected disk: " + choiceDisk.getValue().getName()));
     }
+
 }
